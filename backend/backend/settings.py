533ad14dff4b898a07070ec0671ff13ecfde4c6b
Naddunuri Sanjay <204818477+sanjaynaddunuri@users.noreply.github.com>
2026-02-02 21:09:30 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,4 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    ...
+]
 CORS_ALLOW_ALL_ORIGINS = True
